1b0a600c1d7bbf1e28b2e716867a6956dfa0274f
mehtablocker <46001827+mehtablocker@users.noreply.github.com>
2019-10-05 19:09:54 -0400

    Add files via upload

---
+++ module5.py
@@ -0,0 +1,25 @@
+from flask import Flask, jsonify
+import pandas as pd
+
+app = Flask(__name__)
+
+
+@app.route('/trees/<string:boro>')
+def return_complex(boro):
+    boro = boro.replace(' ', '%20')
+    
+    try:
+        url = "https://data.cityofnewyork.us/resource/nwxe-4ae8.json?$limit=1000000&$where=boroname='" + boro + "'"
+        trees = pd.read_json(url)
+        species_list = pd.Series(trees['spc_common'].unique()).dropna().sort_values().tolist()
+        species_dict = {}
+        for i in species_list:
+            species_dict[i] = boro
+        return jsonify(species_dict)
+    
+    except:
+        return "Check syntax! Choices are only Manhattan, Bronx, Brooklyn, Queens, or Staten Island."
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
